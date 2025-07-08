ad9fa0405db62039111d1ddec7f3ed834603fe1a
Kalllliinaaa <169152002+Kalllliinaaa@users.noreply.github.com>
2025-07-08 19:24:29 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,12 @@
 function isPalindrome(str) {
-  const normalized = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const normalized = str.toLowerCase().replace(/[^a-z0-9а-яё]/gi, ''); // Исправлено: Учитываем кириллицу
   return normalized === normalized.split('').reverse().join('');
 }
 
 function addNumbers(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return NaN;
+  }
   return a + b;
 }
 
